df69014da86dbcdfd75722e2daa88c4af4b11bad
winterweiliao <1030190231@qq.com>
2019-11-12 16:28:53 +0800

    'initial'

---
+++ login/views.py
@@ -0,0 +1,44 @@
+import json
+import base64
+
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+
+from rest_framework.views import APIView
+
+
+# Create your views here.
+def login_page(request):
+    app_url = request.GET.get('app_url')
+    if app_url:
+        return render(request, 'login/login_page.html', {'app_url': app_url})
+    else:
+        return render(request, 'login/login_page.html')
+
+
+def login_success_page(request):
+    app_url = request.GET.get('app_url')
+    token = request.GET.get('token')
+    return render(request, 'login/login_success_page.html', {'app_url': app_url, 'token': token})
+
+
+def login_redirect(request):
+    app_url = request.GET.get('app_url')
+    token = request.GET.get('token')
+    url = '%s?token=%s' % (app_url, token)
+    return redirect(url)
+
+
+def login_logout(request):
+    return redirect('/home/')
+
+
+class LoginSuccessAPIView(APIView):
+    """
+    """
+    def post(self, request, *args, **kwargs):
+        jwt = request.META.get('HTTP_AUTHORIZATION').split(' ')[1]
+        payload = jwt.split(".")[1]
+        payload = base64.urlsafe_b64decode(payload + '=' * (4 - len(payload) % 4))
+        payload = json.loads(payload)
+        return JsonResponse({'token': jwt, 'payload': payload})
